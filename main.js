8f178b95e7cd3b5b36afbe7a31d65224160a33b3
Muhammadamin <fahriddinovmuhammadamin34@gmail.com>
2025-07-01 10:42:16 +0500

    Changed

--- main.js
+++ main.js
@@ -1,81 +1,47 @@
-let door = document.querySelector('.door');
-let bagaj = document.querySelector('.bagaj');
-let zavad = document.querySelector('.zavad');
-let luk = document.querySelector('.luk');
-let kandor = document.querySelector('.kandor');
-let secret = document.querySelector('.secret');
-let img = document.createElement("img");
-img.style.display = "none";
+function dark() {
+    let body = document.querySelector('body')
+    body.classList.toggle('dark')
+    let container = document.querySelector('.container')
+    let black = container.classList.toggle('dark')
+    let img = document.querySelector('.mode-btn')
+    let h1 = document.querySelector('h1')
+    h1.classList.toggle('white')
+    let ul = document.querySelector('ul')
+    ul.classList.toggle('white')
+}
 
-door.addEventListener('click', () => {
-    if (door.textContent == 'peysaj rasmini ochirish') {
-        door.textContent = 'peysaj rasmini korish';
-        img.style.display = "none";
-    } else {
-        door.textContent = 'peysaj rasmini ochirish';
-        img.style.display = "block";
-        img.src = "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=";
-        document.body.appendChild(img);
-    }
-})
+let btn = document.querySelector('.send-btn')
+let malumot = []
 
-bagaj.addEventListener('click', () => {
-    if (bagaj.textContent == 'bagaj ochirish') {
-        bagaj.textContent = 'bagaj korish';
-        img.style.display = "none";
-    } else {
-        bagaj.textContent = 'bagaj ochirish';
-        img.style.display = "block";
-        img.src = "https://images.unsplash.com/photo-1517758478390-c89333af4642?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwc2t5fGVufDB8fDB8fHww";
-        document.body.appendChild(img);
-    }
-})
+btn.addEventListener('click', () => {
+    let input = document.querySelector('.input')
+    let valueInput = input.value
+    if (valueInput !== '') {
+        malumot.push(valueInput)
+        let ul = document.querySelector('ul')
+        let li = document.createElement('li')
+        li.classList = 'list'
+        li.innerHTML = `<input class="check" type="checkbox" /> ${valueInput}
+        <div class="icons">
+            <span class="icon">✏️</span>
+            <span class="delete">🗑</span>
+        </div>`
 
-zavad.addEventListener('click', () => {
-    if (zavad.textContent == 'olov rasmini ochirish') {
-        zavad.textContent = 'olov rasmini korish';
-        img.style.display = "none";
-    } else {
-        zavad.textContent = 'olov rasmini ochirish';
-        img.style.display = "block";
-        img.src = "https://chronicle.brightspotcdn.com/dims4/default/665e255/2147483647/strip/true/crop/10000x4500+0+0/resize/840x378!/quality/90/?url=http%3A%2F%2Fchronicle-brightspot.s3.us-east-1.amazonaws.com%2F6f%2F21%2F0d88b3b74334a1ad8697bb289185%2Fsachs-1129-getty-fb.jpg";
-        document.body.appendChild(img);
-    }
-})
+        let deleteIcon = li.querySelector('.delete')
+        deleteIcon.addEventListener('click', () => {
+            li.remove()
+        })
 
-luk.addEventListener('click', () => {
-    if (luk.textContent == '...') {
-        luk.textContent = 'Americaya';
-        img.style.display = "none";
-    } else {
-        luk.textContent = '...';
-        img.style.display = "block";
-        img.src = "https://preview.redd.it/rlne7n7ttw061.jpg?width=640&crop=smart&auto=webp&s=f10d886f49a875a44b1163e56a729d5fb98e901d";
-        document.body.appendChild(img);
-    }
-})
+        let check = li.querySelector('.check')
+        check.addEventListener('click', () => {
+            li.style.textDecoration = 'line-through'
+            li.style.color = 'grey'
+            check.disabled = true
+        })
 
-kandor.addEventListener('click', () => {
-    if (kandor.textContent == 'ozinni rasmini ochirish') {
-        kandor.textContent = 'ozinni rasmini korish';
-        img.style.display = "none";
+        ul.appendChild(li)
     } else {
-        kandor.textContent = 'ozinni rasmini ochirish';
-        img.style.display = "block";
-        img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSld0Vr3bIRwm8YPCUMdU3ZK6FtlrnNLLm1ag&s";
-        document.body.appendChild(img);
+        alert('Iltimos malumot kiriting!')
     }
-})
-
-secret.addEventListener('click', () => {
-    if (secret.textContent == 'Man etudim') {
-        secret.textContent = 'Don\'t click🤫';
-        img.style.display = "none";
-    } else {
-        secret.textContent = 'Man etudim';
-        img.style.display = "block";
-        img.style.marginTop = "400px";
-        img.src = "https://pbs.twimg.com/media/GGhubSSa0AA_ie9.jpg";
-        document.body.appendChild(img);
-    }
+    input.value = ''    
 })
